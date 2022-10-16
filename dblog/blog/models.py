a5bf2c52714d729a2c904050a324a0b97bcbe252
joyc <joyc@live.jp>
2022-10-16 22:22:12 +0900

    add app blog models

--- dblog/blog/models.py
+++ dblog/blog/models.py
@@ -35,7 +35,7 @@ class Category(models.Model):
         (STATUS_DELETE, '删除'),
     )
     name = models.CharField(max_length=50, verbose_name="名称")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, chioces=STATUS_ITEMS, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     is_nav = models.BooleanField(default=False, verbose_name="是否为导航")
 
     owner = models.ForeignKey(User, verbose_name="作者")
@@ -53,7 +53,7 @@ class Tag(models.Model):
         (STATUS_DELETE, '删除'),
     )
     name = models.CharField(max_length=10, verbose_name="名称")
-    status = models.PositiveIntegerField(default=STATUS_NORMAL, chioces=STATUS_ITEMS, verbose_name="状态")
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者")
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
